b9631fe376b249ed5411858306e07ec2d002f461
Durmuş <demirtasdurmus@gmail.com>
2022-06-20 21:43:57 +0300

    Update Header.jsx

--- client/src/layouts/Header.jsx
+++ client/src/layouts/Header.jsx
@@ -10,22 +10,26 @@ export default function Header() {
     const { user } = useContext(UserContext);
 
     return (
-        <div style={{ width: '100%', height: '5rem', backgroundColor: '#c4c4c4', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <div style={{ width: '100%', height: '5rem', backgroundColor: '#c4c4c4', display: 'flex', alignItems: 'center', justifyContent: 'space-around' }}>
 
-            <Col offset={2} span={2} style={{ fontSize: '1.3rem', fontWeight: 'bold' }}>
+            <Col span={2} style={{ fontSize: '1.3rem', fontWeight: 'bold' }}>
                 <Link to="/">Home</Link>
             </Col>
 
-            <Col offset={2} span={2} style={{ fontSize: '1.3rem', fontWeight: 'bold' }}>
+            <Col span={2} style={{ fontSize: '1.3rem', fontWeight: 'bold' }}>
                 <Link to="/contact">Contact</Link>
             </Col>
 
-            <Col offset={2} span={2} style={{ fontSize: '1.3rem', fontWeight: 'bold' }}>
+            <Col span={2} style={{ fontSize: '1.3rem', fontWeight: 'bold' }}>
                 <Link to="/about">About</Link>
             </Col>
 
-            <Col offset={2} span={4} style={{ fontSize: '1.3rem', fontWeight: 'bold', display: 'flex', flexDirection: 'row' }}>
-                {user && user.id ? `${user.firstName} ${user.lastName}` : <Link to="/sign-in">Sign In</Link>}
+            <Col span={2} style={{ fontSize: '1.3rem', fontWeight: 'bold' }}>
+                <Link to="/skills">Skills</Link>
+            </Col>
+
+            <Col span={5} style={{ fontSize: '1.3rem', fontWeight: 'bold', display: 'flex', flexDirection: 'row', justifyContent: "end" }}>
+                {user && user.id ? <h4 style={{ margin: "auto" }}>{user.firstName} {user.lastName}</h4> : <Link style={{ margin: "auto" }} to="/sign-in">Sign In</Link>}
                 {user && user.id ? <img src={user.profileImg} alt="profileImg" style={{ borderRadius: "55px", height: "4rem" }} /> : null}
                 {(user && user.id) && <button onClick={logoutUser} style={{ marginLeft: "2rem", borderRadius: "20px", border: "none" }}>Logout</button>}
             </Col>
