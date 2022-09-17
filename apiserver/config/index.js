e44d1451ed2706a64e5fe1379d6a816c3675ed74
Durmuş <demirtasdurmus@gmail.com>
2022-09-17 23:41:00 +0300

    Update index.js

--- apiserver/config/index.js
+++ apiserver/config/index.js
@@ -1,19 +1,19 @@
 var PORT,
     API_URL,
-    CLIENT_URL;
+    CLIENT_URL
 
 if (process.env.NODE_ENV === 'production') {
-    PORT = process.env.PORT_PROD;
-    API_URL = process.env.API_URL_PROD;
-    CLIENT_URL = process.env.CLIENT_URL_PROD;
+    PORT = process.env.PORT_PROD
+    API_URL = process.env.API_URL_PROD
+    CLIENT_URL = process.env.CLIENT_URL_PROD
 } else if (process.env.NODE_ENV === 'test') {
-    PORT = process.env.PORT_TEST;
-    API_URL = process.env.API_URL_TEST;
-    CLIENT_URL = process.env.CLIENT_URL_TEST;
+    PORT = process.env.PORT_TEST
+    API_URL = process.env.API_URL_TEST
+    CLIENT_URL = process.env.CLIENT_URL_TEST
 } else {
-    PORT = process.env.PORT_DEV;
-    API_URL = "http://localhost:" + PORT;
-    CLIENT_URL = process.env.CLIENT_URL_DEV;
-};
+    PORT = process.env.PORT_DEV
+    API_URL = "http://localhost:" + PORT
+    CLIENT_URL = process.env.CLIENT_URL_DEV
+}
 
-module.exports = { API_URL, CLIENT_URL, PORT };
+module.exports = { API_URL, CLIENT_URL, PORT }
