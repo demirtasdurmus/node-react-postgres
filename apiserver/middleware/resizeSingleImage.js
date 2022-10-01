2793cf857243b9557ad6cadd554bdf8e4a2e2ee7
Durmuş <demirtasdurmus@gmail.com>
2022-10-02 02:01:52 +0300

    FIX issues

--- apiserver/middleware/resizeSingleImage.js
+++ apiserver/middleware/resizeSingleImage.js
@@ -1,22 +1,20 @@
-const { v4: uuidv4 } = require('uuid');
-const AppError = require('../utils/appError');
-const catchAsync = require('../utils/catchAsync');
-const sharp = require('sharp');
+const { v4: uuidv4 } = require('uuid')
+const catchAsync = require('../utils/catchAsync')
+const sharp = require('sharp')
 
 
 module.exports = (name, { width = 500, height = 500, quality = 90, format = "jpeg" }) => {
     return catchAsync(async (req, res, next) => {
-        if (!req.file) return next();
-        //console.log("first,", req.file);
+        if (!req.file) return next()
 
-        req.file.filename = `${name}-${uuidv4()}.${format}`;
+        req.file.filename = `${name}-${uuidv4()}.${format}`
 
         await sharp(req.file.buffer)
             .resize(width, height)
             .toFormat(format)
             .jpeg({ quality: quality })
-            .toFile(`${process.env.IMAGES_DIR}/${req.file.filename}`);
+            .toFile(`${process.env.IMAGES_DIR}/${req.file.filename}`)
 
-        next();
+        next()
     })
-};
+}
--- apiserver/utils/session.js
+++ apiserver/utils/session.js
@@ -1,41 +1,23 @@
-const crypto = require("crypto")
-const { promisify } = require('util')
+const { createCipheriv, createDecipheriv, scryptSync } = require('crypto')
 const AppError = require('./appError')
+const key = scryptSync(process.env.SESSION_ENCRYPT_SECRET, 'salt', 24)
+const iv = Buffer.alloc(16, 0) // Initialization crypto vector
 
 
-exports.encrypt = async (token) => {
-    // check if token exists and is a string
-    if (!token || typeof token !== 'string') throw new AppError(500, "Invalid token to encrypt")
-    // encrypt the token with Base64
-    const encryptedToken = Buffer.from(token).toString('base64')
-    // create a random string and encrypt to prefix the encrypted token
-    const randomStringPrefix = await getRandomBytes(10)
-    const encryptedPrefix = Buffer.from(randomStringPrefix).toString('base64')
-    const configuredPrefix = encryptedPrefix.slice(0, encryptedPrefix.length - 1) // 27 chars long
-    // create a random string and encrypt to inject in the middle of the encrypted token
-    const randomStringMiddle = await getRandomBytes(20)
-    const encryptedMiddle = Buffer.from(randomStringMiddle).toString('base64')
-    const configuredMiddle = encryptedMiddle.slice(0, encryptedMiddle.length - 2) // 54 chars long
-    // create seesion session
-    const session = `${configuredPrefix}${encryptedToken.slice(0, 15)}${configuredMiddle}${encryptedToken.slice(15, encryptedToken.length)}`
-    return session
+exports.encrypt = (token) => {
+    const cipher = createCipheriv(process.env.SESSION_ALGORITHM, key, iv)
+    let encrypted = cipher.update(token, 'utf8', 'hex')
+    encrypted += cipher.final('hex')
+    return encrypted
 }
 
 exports.decrypt = (session) => {
-    // check if token exists and is a string, and has the min length to be decrypted
-    if (typeof session !== 'string' || session.length < 43) throw new AppError(401, "Invalid session")
-    // extract encrypted token from the session
-    const unprefixedSession = session.slice(27, session.length)
-    const sanitizedSession = unprefixedSession.slice(0, 15) + unprefixedSession.slice(69, unprefixedSession.length)
-    // decrypt the token to ascii
-    const token = Buffer.from(sanitizedSession, 'base64').toString('ascii')
-    return token
-}
-
-
-/*
-UTILITY FUNCTIONS
-*/
-const getRandomBytes = async (size) => {
-    return (await promisify(crypto.randomBytes)(size)).toString('hex')
+    try {
+        const decipher = createDecipheriv(process.env.SESSION_ALGORITHM, key, iv)
+        let decrypted = decipher.update(session, 'hex', 'utf8')
+        decrypted += decipher.final('utf8')
+        return decrypted
+    } catch (error) {
+        throw new AppError(401, "Unauthorized")
+    }
 }
